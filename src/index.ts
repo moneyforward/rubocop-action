071b055e18b366207b8a3c435c648e4f96415516
Naoki Kimura <n.kimura.cap@gmail.com>
2020-04-27 15:06:21 +0900

    Fix a bug

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Analyzer from './analyzer';
 
 (async (): Promise<void> => {
   const options = JSON.parse(process.env.INPUT_OPTIONS || '[]');
-  const startingPoints = process.env.INPUT_STARTING_POINTS;
+  const startingPoints = process.env.INPUT_STARTING_POINTS || '.';
   const workingDirectory = process.env.INPUT_WORKING_DIRECTORY;
   workingDirectory && process.chdir(workingDirectory);
   const analyzer = new Analyzer(options);
